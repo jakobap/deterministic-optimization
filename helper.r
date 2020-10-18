4d272a9a3233ac2e3f4e5660eda745b1f67041f7
EGimenez <eduardgimenez@yahoo.com>
2020-10-18 03:57:37 +0200

    Finished ;-)

--- helper.r
+++ helper.r
@@ -2,7 +2,7 @@ source('new_life_nice_easy.r')
 source('not_so_easy.r')
 library(MASS)
 
-set.seed(1)
+#set.seed(1)
 
 n <- 5     ## Dimension of matrix
 S <- matrix(rnorm(n*n), ncol=n)
@@ -14,4 +14,6 @@ K <- diag(n)
 print('exercise_2')
 Kr_2 <- exercise_2(S, pre=1e-2, max_iter = 1000)
 print('exercise_3')
-Kr_3 <- exercise_3(S, pre=1e-2, max_iter = 1000)
+Kr_3 <- exercise_3(S, pre=1e-2, max_iter = 100)
+print(Kr_2)
+print(Kr_3)
